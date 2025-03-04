a736ae65eba298a78f0fd85f220af3958107b357
Michael Corcias <michael.corcias@gelt.com>
2025-03-04 23:20:07 +0100

    entregable

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,39 @@
 import './App.css'
+import { useState } from 'react';
 
 function App() {
- //Aquí tu código
+    const [name, setName] = useState("Sofía");
+    const [newName, setNewName] = useState("");
+
+    const changeName = (event) => {
+        event.preventDefault();
+
+        setName(newName);
+        setNewName("");
+        
+    };  
+
+    return (
+        
+        <div>
+            <h2>Teacher Name: {name}</h2>
+            <ul>
+                <li onClick={() => setName("Data")}>Data</li>
+                <li onClick={() => setName("Reyes")}>Reyes</li>
+                <li onClick={() => setName("Yolanda")}>Yolanda</li>
+            </ul>
+        
+            <form onSubmit={changeName}>
+            <input 
+                type="text" 
+                value={newName} 
+                onChange={(e) => setNewName(e.target.value)} 
+                placeholder="Add a name"
+            />
+            <button type="submit">Add</button>
+            </form>
+        </div>
+    );
 }
 
 export default App
